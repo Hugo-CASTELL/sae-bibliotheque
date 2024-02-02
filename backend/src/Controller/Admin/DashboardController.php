a00810d87796ab2a2d83dff11c319fa3a761c5b6
matthieurobert <matthieurobert82@gmail.com>
2024-02-02 10:03:28 +0100

    feat: setup auth (#21)

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Utilisateur;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -26,5 +27,6 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', Utilisateur::class);
     }
 }
