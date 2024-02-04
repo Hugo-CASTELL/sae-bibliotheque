b6bc7d710aff01165584df15347c4efcfb66f020
Matthieu Robert <matthieurobert82@gmail.com>
2024-02-04 11:40:57 +0100

    fix #26

--- backend/src/Entity/Emprunt.php
+++ backend/src/Entity/Emprunt.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EmpruntRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EmpruntRepository::class)]
+#[ApiResource()]
 class Emprunt
 {
     #[ORM\Id]
