bdb8be585911d9eb69bd669fbe2ace013d8a9bf1
Matthieu Robert <matthieurobert82@gmail.com>
2024-02-05 14:32:12 +0100

    feat: create update route for adherent

--- backend/src/Controller/Api/UserController.php
+++ backend/src/Controller/Api/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Api;
 
 use App\Repository\AdherentRepository;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
@@ -21,6 +24,41 @@ class UserController extends AbstractController
         if (!$adherent) {
             return $this->json(['message' => 'Adherent not found'], 404);
         }
+
+        return $this->json($adherent, 200, [], ['groups' => 'adherent:read']);
+    }
+
+    #[Route('/api/user/me/update', name: 'app_api_user_update', methods: ['PUT'])]
+    public function update(Request $request, EntityManagerInterface $entityManager, AdherentRepository $adherentRepository, UtilisateurRepository $utilisateurRepository): JsonResponse {
+        /** @var \App\Entity\Utilisateur $user */
+        $user = $this->getUser();
+
+        $adherent = $adherentRepository->findOneBy(['email' => $user->getEmail()]);
+        $utilisateur = $utilisateurRepository->findOneBy(['email' => $user->getEmail()]);
+
+        if (!$adherent) {
+            return $this->json(['message' => 'Adherent not found'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['adressePostale'])) {
+            $adherent->setAdressePostale($data['adressePostale']);
+        }
+        if (isset($data['numTel'])) {
+            $adherent->setNumTel($data['numTel']);
+        }
+        if (isset($data['photo'])) {
+            $adherent->setPhoto($data['photo']);
+        }
+        if (isset($data['email'])) {
+            $adherent->setEmail($data['email']);
+            $utilisateur->setEmail($data['email']);
+        }
+
+        $entityManager->persist($adherent);
+        $entityManager->persist($utilisateur);
+        $entityManager->flush();
 
         return $this->json($adherent, 200, [], ['groups' => 'adherent:read']);
     }
