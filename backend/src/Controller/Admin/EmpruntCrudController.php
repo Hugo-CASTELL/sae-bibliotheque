6369773c54be9145b266607123e6258a24c9a5c8
Hugo-CASTELL <hugo.castell@outlook.fr>
2024-02-05 16:00:22 +0100

    Persist methods on Emprunt and Reservations

--- backend/src/Controller/Admin/EmpruntCrudController.php
+++ backend/src/Controller/Admin/EmpruntCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Emprunt;
+use DateInterval;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -16,15 +20,51 @@ class EmpruntCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $currentDate = new DateTimeImmutable();
+        $twoWeeks = new DateInterval('P15D');
+        $twoWeeksLater = $currentDate->add($twoWeeks);
+
         return [
             DateTimeField::new('dateEmprunt')
-                ->setLabel('Date d\'emprunt'),
+                ->setLabel('Date d\'emprunt')
+                ->setRequired(true)
+                ->setFormat('dd/MM/yyyy')
+                ->setValue($currentDate),
             DateTimeField::new('dateRetour')
-                ->setLabel('Date de retour'),
+                ->setLabel('Date de retour')
+                ->setRequired(true)
+                ->setFormat('dd/MM/yyyy')
+                ->setValue($twoWeeksLater),
             AssociationField::new('adherent')
-                ->setLabel('Adhérent'),
+                ->setLabel('Adhérent')
+                ->setRequired(true),
             AssociationField::new('livre')
-                ->setLabel('Livre'),
+                ->setLabel('Livre')
+                ->setRequired(true),
         ];
     }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entity): void
+    {
+        // Récupération du livre
+        $livre = $entity->getLivre();
+        $livre->setDisponible(false);
+
+        // Suppression de la réservation si elle existe
+        $reservationsAdherent = $entity->getAdherent()->getReservations();
+        foreach ($reservationsAdherent as $reservation) {
+            if ($reservation->getLivre() === $livre) {
+                $entityManager->remove($reservation);
+                $entityManager->persist($reservation);
+                break;
+            }
+        }
+
+        // Persist et flush
+        $entityManager->persist($livre);
+        $entityManager->persist($entity);
+        $entityManager->flush();
+    }
+
 }
+
--- backend/src/Controller/Admin/ReservationsCrudController.php
+++ backend/src/Controller/Admin/ReservationsCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Reservations;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -16,13 +18,48 @@ class ReservationsCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $currentDate = new DateTimeImmutable();
+
         return [
             DateTimeField::new('dateResa')
-                ->setLabel('Date de réservation'),
+                ->setLabel('Date de réservation')
+                ->setRequired(true)
+                ->setFormat('dd/MM/yyyy')
+                ->setValue($currentDate),
             AssociationField::new('adherent')
-                ->setLabel('Adhérent'),
+                ->setLabel('Adhérent')
+                ->setRequired(true),
             AssociationField::new('livre')
-                ->setLabel('Livre'),
+                ->setLabel('Livre')
+                ->setRequired(true),
         ];
     }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entity): void
+    {
+        // Récupération du livre et de l'adherent
+        $livre = $entity->getLivre();
+        $adherent = $entity->getAdherent();
+
+        // Vérification de la disponibilité du livre
+        if ($livre->getDisponible() === false) {
+            $this->addFlash('danger', 'Le livre n\'est pas disponible');
+            return;
+        }
+
+        // Si l'adherent a déjà trois réservations
+        // if ($adherent->getReservations()->count() >= 3) {
+        //     $this->addFlash('danger', 'L\'adherent a déjà trois réservations');
+        //     return;
+        // }
+
+        // Traitement du livre
+        $livre->setDisponible(false);
+
+        // Persist et flush
+        $entityManager->persist($livre);
+        $entityManager->persist($entity);
+        $entityManager->flush();
+    }
+
 }
