94b248cc5a334cb0aed0175fe26dcd0ac35a26d8
Thomas Testa <thomastesta@orange.fr>
2024-02-07 14:24:45 +0100

    test
    fail

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms';
 import { LivresListComponent } from './livres-list/livres-list.component';
 import { LoginComponent } from './login/login.component';
 import { AccountComponent } from './account/account.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LivresListComponent,
     LoginComponent,
-    AccountComponent
+    AccountComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
