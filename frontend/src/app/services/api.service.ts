84c389fb4da576af0a6d6e8709e02f6a0afbf214
Hugo-CASTELL <hugo.castell@outlook.fr>
2024-02-09 16:06:15 +0100

    Hotfix emprunt endpoint

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -10,7 +10,6 @@ import { outputLogin } from '../models/api/output/outputLogin';
 import { Categorie } from '../models/categorie';
 import { Adherent } from '../models/adherent';
 import { Reservations } from '../models/reservations';
-import { Emprunt } from '../models/emprunt';
 
 @Injectable({
   providedIn: 'root'
@@ -29,10 +28,6 @@ export class ApiService {
 
   getUser(): Observable<any> {
     return this.http.get<Adherent>(`${this.apiUrl}/user/me`, this.getHttpHeader());
-  }
-
-  getEmprunt(id: number): Observable<Emprunt> {
-    return this.http.get<Emprunt>(`${this.apiUrl}/emprunts/${id}`);
   }
 
   getLivre(id: number): Observable<Livre> {
